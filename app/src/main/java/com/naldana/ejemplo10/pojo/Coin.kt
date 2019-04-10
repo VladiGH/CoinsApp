d4a245bf858821586ec99fee6f22f9fc6ab51604
Unknown <shulkmaster@gmail.com>
2019-04-09 21:07:39 -0600

    Medio poniendo todo en orden
    A saber que hice

--- app/src/main/java/com/naldana/ejemplo10/pojo/Coin.kt
+++ app/src/main/java/com/naldana/ejemplo10/pojo/Coin.kt
@@ -6,22 +6,21 @@ import android.os.Parcelable
 data class Coin (
     val name:String = "N/A",
     val country:String = "N/A",
-    val ano: String = "N/A",
-    val avaliable:Boolean = true
+    val year: String = "N/A",
+    val available:Boolean = true
     ): Parcelable {
     constructor(parcel: Parcel) : this(
-        name = parcel.readString(),
-        country = parcel.readString(),
-        ano = parcel.readString(),
-        avaliable = parcel.readByte() != 0.toByte()
-    ) {
-    }
+        name = parcel.readString()!!,
+        country = parcel.readString()!!,
+        year = parcel.readString()!!,
+            available = parcel.readByte() != 0.toByte()
+    )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeString(country)
-        parcel.writeString(ano)
-        parcel.writeByte(if (avaliable) 1 else 0)
+        parcel.writeString(year)
+        parcel.writeByte(if (available) 1 else 0)
     }
 
     override fun describeContents(): Int {
