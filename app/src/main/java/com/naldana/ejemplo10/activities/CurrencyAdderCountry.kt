ee794ac3520942ae2ccf02505ea8022198183d21
Jorge <valencia12@github.com>
2019-05-13 14:48:54 -0600

    Nuevos cambios, metodo casi añadido

--- app/src/main/java/com/naldana/ejemplo10/activities/CurrencyAdderCountry.kt
+++ app/src/main/java/com/naldana/ejemplo10/activities/CurrencyAdderCountry.kt
@@ -2,6 +2,7 @@ package com.naldana.ejemplo10.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.naldana.ejemplo10.R
 import com.naldana.ejemplo10.database.RealTimeDatabase
 
@@ -12,6 +13,17 @@ class CurrencyAdderCountry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency_adder2)
+        findViewById<Button>(R.id.submit1).setOnClickListener{
+            //database.insertData(gater()){
+
+        }
     }
 
+    private fun gatter(){
+
+    }
+
+
 }
+
+
