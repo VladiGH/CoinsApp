42806b86c45b108fa44b33840818e5fbdd2c66d3
jorge <00003417@uca.edu>
2019-05-13 21:40:35 -0600

    Agregando un boton y un metodo parfa insertar paises, pero se bugea :V so close

--- app/src/main/java/com/naldana/ejemplo10/activities/CurrencyAdderCountry.kt
+++ app/src/main/java/com/naldana/ejemplo10/activities/CurrencyAdderCountry.kt
@@ -2,9 +2,12 @@ package com.naldana.ejemplo10.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.widget.Button
+import android.widget.EditText
 import com.naldana.ejemplo10.R
 import com.naldana.ejemplo10.database.RealTimeDatabase
+import com.naldana.ejemplo10.models.Country
 
 class CurrencyAdderCountry : AppCompatActivity() {
 
@@ -14,14 +17,26 @@ class CurrencyAdderCountry : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currency_adder2)
         findViewById<Button>(R.id.submit1).setOnClickListener{
-            //database.insertData(gater()){
-
+            database.insertData(gatter()){ success, msm ->
+                Snackbar.make(
+                        it,
+                        when {
+                            success -> getString(R.string.fb_ok)
+                            msm == "" -> getString(R.string.fb_error)
+                            else -> msm
+                        },
+                        Snackbar.LENGTH_LONG
+                    ).show()
+            }
         }
     }
+    private fun gatter(): Country = Country (
+        null,
+        findViewById<EditText>(R.id.editText_name1).text.toString().toLowerCase(),
+        findViewById<EditText>(R.id.editText_country1).text.toString().toLowerCase()
+    )
 
-    private fun gatter(){
 
-    }
 
 
 }
